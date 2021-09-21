3368c8ed9e0a66309e65f47c2839f910241c80ea
aliabb <ali3abbasov@gmail.com>
2021-09-21 23:48:04 +0300

    add: Vue install. Example App Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+
+createApp(App).mount("#app");
+
 require('./bootstrap');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h4 class="text-center">Hello World!</h4>
+        <h5>Hello</h5>
+        <p>Count is {{ count }}</p>
+        <button @click="count--">-</button>
+            {{ count }}
+        <button @click="count++">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
