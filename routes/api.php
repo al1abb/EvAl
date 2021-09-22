17cd8336785a943d4ccd5f7383f27194af346348
aliabb <ali3abbasov@gmail.com>
2021-09-23 00:01:34 +0300

    add: Initial get api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+
+// move "delete" to web.php as it's not an api route
+Route::delete('/post/{id}', [PostController::class, 'destroy']);
