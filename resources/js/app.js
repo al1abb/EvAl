95663b54799b20939772e0613fc6e7332277975a
aliabb <ali3abbasov@gmail.com>
2021-09-23 16:21:52 +0300

    add: Import router and use it in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import router from "./routes";
 
 import App from './components/App.vue';
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
 
 require('./bootstrap');
