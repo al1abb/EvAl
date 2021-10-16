fd5cf50be79263c3149ca964e513b34d328c52ce
aliabb <ali3abbasov@gmail.com>
2021-10-16 17:00:32 +0300

    add: Single database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            AgencySeeder::class,
+            UserSeeder::class,
+            PostSeeder::class,
+            FlagSeeder::class,
+        ]);
     }
 }
