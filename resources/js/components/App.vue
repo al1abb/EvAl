c4bdfcffcd8c17525d4a18400ea9da7a9f4091d1
aliabb <ali3abbasov@gmail.com>
2021-10-16 17:11:47 +0300

    add: App Component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,20 +1,49 @@
 <template>
-    <div>Hello World</div>
+    <v-app
+        class="appMain"
+    >
+        <v-container>
+            <v-row
+                class="text-center"
+            >
+                <v-col>
+                    <p><router-link to="/">Home</router-link></p>
+                </v-col>
+                <v-col>
+                    <p><router-link to="/test">Test</router-link></p>
+                </v-col>
+            </v-row>
+            <v-btn
+                elevation="2"
+            >
+                Hello
+            </v-btn>
+        </v-container>
 
-    <router-link to="/">Home</router-link>
-    <router-link to="/test">Test</router-link>
-
-    <router-view v-slot="{ Component }">
-        <component :is="Component" />
-    </router-view>
+        <router-view v-slot="{ Component }">
+            <component :is="Component" />
+        </router-view>
+    </v-app>
 </template>
 
 <script>
 export default {
-
+    
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap');
 
+
+p {
+    margin: 0px !important;
+    padding: 0px !important;
+}
+
+.appMain {
+    background-color: #f7f2f2 !important;
+
+    font-family: 'Noto Sans', sans-serif;
+}
 </style>
