00732df93cf22ac680fac2fb8eb5c71b6cf90392
aliabb <ali3abbasov@gmail.com>
2021-10-16 17:03:09 +0300

    add: Agency api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AgencyController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Models\Agency;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,5 +32,12 @@ Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// Agencies api routes
+Route::get('/agencies', [AgencyController::class, 'index']);
+Route::post('/agencies', [AgencyController::class, 'store']);
+Route::get('/agencies/{id}', [AgencyController::class, 'show']);
+Route::put('/agencies/{id}', [AgencyController::class, 'update']);
+Route::delete('/agencies/{id}', [AgencyController::class, 'destroy']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/post/{id}', [PostController::class, 'show']);
