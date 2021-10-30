87388138b0d735215ebe09f5af9360c47fc1bbe2
Ali <ali3abbasov@gmail.com>
2021-10-30 22:23:17 +0300

    refactor: Add PostSection component to home component, Home component refactor

---
+++ resources/js/components/PostSection.vue
@@ -0,0 +1,69 @@
+<template>
+    <section>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+
+            <div v-show="!loading" 
+                class="d-flex justify-content-center align-items-center mb-4" 
+                style="background-color: white; height: 5rem; width: 70%; text-align: center; border-radius: 5px;"
+            >
+                <span class="section-header">{{ title }}</span>
+            </div>
+
+            <div style="width: 60%;">
+                <div style="display: flex; flex-wrap: wrap; justify-content: center; gap: 1rem;">
+
+                    <div class="d-flex justify-content-center" v-for="(listing, i) in responseData" :key="i">
+                        <v-skeleton-loader
+                            :loading="loading"
+                            type="card"
+                            min-width="14.5rem"
+                            max-width="14.5rem"
+                        >
+                            <Post 
+                                :id="listing.id"
+                                :user="listing.user"
+                                :agency="listing.agency"
+                                :type="listing.estate_type"
+                                :city="listing.city"
+                                :area="listing.area"
+                                :roomCount="listing.room_count"
+                                :address="listing.address"
+                                :district="listing.district"
+                                
+                                :apartmentFloor="listing.apartment_floor"
+                                :totalFloors="listing.total_floors"
+                                
+                                :price="listing.price"
+                                :isVip="listing.is_vip"
+                                :hasVoucher="listing.has_voucher"
+                                
+                                :createdAt="listing.created_at"
+                                :loading="loading"
+                            />
+                        </v-skeleton-loader>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+</template>
+
+<script>
+import Post from '../components/Post.vue';
+
+export default {
+    props: ["title", "responseData", "loading"],
+    components: { Post }
+}
+</script>
+
+<style>
+.section-header {
+    font-size: 1.0rem;
+    font-weight: 600;
+}
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,135 +1,32 @@
 <template>
 	<div>
-        <div class="text-center">
-            This is home page
-        </div>
 
-        <div class="text-center">
+        <div class="text-center mb-3">
             Current page is {{ currentPage }}
         </div>
 
-        <section class="">
-            <!-- v-container had these classes d-flex flex-wrap -->
-            <div class="d-flex flex-column justify-content-center align-items-center">
-                <div v-show="!loading" 
-                    class="d-flex justify-content-center align-items-center mb-4" 
-                    style="background-color: white; height: 5rem; width: 70%; text-align: center; border-radius: 5px;"
-                >
-                    <span class="section-header">ALL POSTS</span>
-                </div>
-
-                <!-- Simple initial pagination -->
-                <!-- <div class="d-flex justify-content-between mb-3" v-show="!loading">
-                    <v-btn v-if="!isFirstPage" :disabled="loading" @click="prevPage"> Back </v-btn>
-                    <v-btn v-if="!isLastPage" :disabled="loading" @click="nextPage"> > </v-btn>        
-                </div> -->
-
-                <v-pagination
-                    v-model="currentPage"
-                    :length="lastPage"
-                    :disabled="loading"
-                    class="mb-3"
-                >
-                </v-pagination>
-
-                <div style="width: 60%;">
-                    <div style="display: flex; flex-wrap: wrap; justify-content: space-around; gap: 1rem;">
-                        <div class="d-flex justify-content-center" v-for="(listing, i) in responseData" :key="i"
-                            lg="3"
-                            md="6"
-                            sm="12"
-                        >
-                            <v-skeleton-loader
-                                :loading="loading"
-                                type="card"
-                                min-width="14.5rem"
-                                max-width="14.5rem"
-                            >
-                                <Post 
-                                    :id="listing.id"
-
-                                    :user="listing.user"
-                                    :agency="listing.agency"
-
-                                    :type="listing.estate_type"
-
-                                    :city="listing.city"
-                                    :area="listing.area"
-                                    :roomCount="listing.room_count"
-                                    :address="listing.address"
-                                    :district="listing.district"
-                                    
-                                    :apartmentFloor="listing.apartment_floor"
-                                    :totalFloors="listing.total_floors"
-                                    
-                                    :price="listing.price"
-
-                                    :isVip="listing.is_vip"
-                                    :hasVoucher="listing.has_voucher"
-                                    
-                                    :createdAt="listing.created_at"
-                                    :loading="loading"
-                                />
-                            </v-skeleton-loader>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
+        <PostSection 
+            title="VIP Posts"
+            :responseData="vipData"
+            :loading="loading"
+        />
 
         <hr style="border: none; margin-top: 5rem; margin-bottom: 5rem;">
 
-        <section>
-            <div class="d-flex flex-column justify-content-center align-items-center">
-                <div v-show="!loading" 
-                    class="d-flex justify-content-center align-items-center mb-4" 
-                    style="background-color: white; height: 5rem; width: 70%; text-align: center; border-radius: 5px;"
-                >
-                    <span class="section-header">VIP POSTS</span>
-                </div>
+        <PostSection 
+            title="All Posts"
+            :responseData="allData"
+            :loading="loading"
+        />
 
-                <div style="width: 60%;">
-                    <div style="display: flex; flex-wrap: wrap; justify-content: space-around; gap: 1rem;">
-                        <div class="d-flex justify-content-center" v-for="(listing, i) in vipData" :key="i">
-                            <v-skeleton-loader
-                                :loading="loading"
-                                type="card"
-                                min-width="14.5rem"
-                                max-width="14.5rem"
-                            >
-                                <Post 
-                                    :id="listing.id"
+        <v-pagination
+            v-model="currentPage"
+            :length="lastPage"
+            :disabled="loading"
+            class="my-5"
+        >
+        </v-pagination>
 
-                                    :user="listing.user"
-                                    :agency="listing.agency"
-
-                                    :type="listing.estate_type"
-
-                                    :city="listing.city"
-                                    :area="listing.area"
-                                    :roomCount="listing.room_count"
-                                    :address="listing.address"
-                                    :district="listing.district"
-                                    
-                                    :apartmentFloor="listing.apartment_floor"
-                                    :totalFloors="listing.total_floors"
-                                    
-                                    :price="listing.price"
-
-                                    :isVip="listing.is_vip"
-                                    :hasVoucher="listing.has_voucher"
-                                    
-                                    :createdAt="listing.created_at"
-                                    :loading="loading"
-                                />
-                            </v-skeleton-loader>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        
     </div>
 </template>
 
@@ -137,12 +34,13 @@
 import axios from 'axios';
 
 import Post from '../components/Post.vue';
+import PostSection from '../components/PostSection.vue';
 
 export default {
-	components: { Post },
+	components: { Post, PostSection },
 	data() {
         return {
-            responseData: [],
+            allData: [],
             vipData: [],
             currentPage: 1,
             loading: true,
@@ -158,7 +56,7 @@ export default {
             axios.get('/api/posts?page=' + this.currentPage)
                 .then((response) => {
                     console.log(response.data) // chaining 'data' to this fixes bug
-                    this.responseData = response.data.data
+                    this.allData = response.data.data
                     this.currentPage = response.data.current_page
                     this.isFirstPage = response.data.prev_page_url ? false : true
                     this.isLastPage = response.data.next_page_url ? false : true
@@ -181,6 +79,12 @@ export default {
                     console.log(response.data)
                     this.vipData = response.data.data
                 })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    this.loading = false
+                })
         },
 
         prevPage() {
@@ -193,8 +97,8 @@ export default {
         }
     },
     mounted() {
-        this.handleListings()
         this.handleVipPosts()
+        this.handleListings()
     },
     watch: {
         currentPage: function() {
@@ -205,8 +109,5 @@ export default {
 </script>
 
 <style>
-.section-header {
-    font-size: 1.0rem;
-    font-weight: 600;
-}
+
 </style>
