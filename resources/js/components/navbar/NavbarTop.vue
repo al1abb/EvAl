d6d3656a56a5d23ec84de5b3965ae7ecaa428da5
Ali Abbasov <ali3abbasov@gmail.com>
2021-11-03 13:46:38 +0200

    add: ProfileMenu component, refactor NavbarTop

--- resources/js/components/navbar/NavbarTop.vue
+++ resources/js/components/navbar/NavbarTop.vue
@@ -1,26 +1,40 @@
 <template>
     <div
-        class="d-flex justify-content-end align-items-center py-1"
+        class="d-flex justify-content-end align-items-center p-2"
         style="background-color: #FFFFFF;"
     >
-        <router-link
-            to="/daxil-ol"
-            class="no-uppercase reset_word_spacing text-success p-2"
-        >
-            Daxil ol
-        </router-link>
+        
+        <ProfileMenu :auth="authenticated" :user="user" />
 
-        <router-link
-            to="/qeydiyyat"
-            class="no-uppercase reset_word_spacing text-danger p-2"
-        >
-            Qeydiyyat
-        </router-link>
+        <div v-if="!authenticated">
+            <router-link
+                to="/daxil-ol"
+                class="no-uppercase reset_word_spacing text-success p-2"
+            >
+                Daxil ol
+            </router-link>
+
+            <router-link
+                to="/qeydiyyat"
+                class="no-uppercase reset_word_spacing text-danger p-2"
+            >
+                Qeydiyyat
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+import ProfileMenu from '../ProfileMenu.vue';
+export default {
+    components: { ProfileMenu },
+    computed: {
+        ...mapState(["authenticated", "user"])
+    }
+};
 </script>
 
-<style></style>
+<style>
+
+</style>
