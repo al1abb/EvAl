fd8b7902ecd633115bc9416e9da0ec53609c6d54
Ali Abbasov <ali3abbasov@gmail.com>
2021-11-05 13:00:50 +0200

    add: Empty baseUrl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,6 +24,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = '';
 
 
 /**
