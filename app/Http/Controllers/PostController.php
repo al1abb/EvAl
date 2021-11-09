61cb2e26b0911fa7cf532e2034b2946171ee6112
Ali Abbasov <ali3abbasov@gmail.com>
2021-11-09 10:58:58 +0200

    add: Initial search function in PostController
    This commit utilizes scope functions from Post model to search for
    data within specific query specified in Post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(20);
+        $posts = Post::inRandomOrder()->paginate(20);
 
         $posts->map(function($post) {
             $post['user'] = [$post->user];
@@ -27,7 +27,7 @@ class PostController extends Controller
      * Display a listing of vip resource.
      */
     public function vipPosts() {
-        $posts = Post::where('is_vip', true)->paginate(20);
+        $posts = Post::vipPosts()->latest()->paginate(20);
 
         $posts->map(function($post) {
             $post['user'] = [$post->user];
@@ -93,5 +93,88 @@ class PostController extends Controller
         $post->delete();
 
         return response()->json([], 204, [/*headers here*/], JSON_PRETTY_PRINT);
+    }
+
+    public function searchPost(Request $request) {
+
+        $start = microtime(true);
+        $fields = $request->validate([
+            'tradeType' => 'required|string',
+            'estateType' => 'string',
+            'roomCount' => '',
+            'city' => 'string',
+            'priceMin' => 'integer',
+            'priceMax' => 'integer'
+        ]);
+
+        if($fields['tradeType'] == 'Hamısı') {
+            $tradeType = ['rent', 'sell'];
+        } else {
+            if($fields['tradeType'] == 'Alış') {
+                $tradeType = ['sell'];
+            }
+            else {
+                $tradeType = ['rent'];
+            }
+        }
+
+        // if($fields['tradeType'] == 'Alış') {
+        //     $fields['tradeType'] = 'sell';
+        // }
+        // else {
+        //     $fields['tradeType'] = 'rent';
+        // }
+        
+        $estateType = $fields['estateType'] == 'Mənzil' ? 'apartment' : ($fields['estateType'] == 'Ev-Villa' ? 'house_villa' : ($fields['estateType'] == 'Ofis' ? 'office' : ($fields['estateType'] == 'Qaraj' ? 'garage' : ($fields['estateType'] == 'Torpaq' ? 'land' : 'err'))));
+
+        if($fields['roomCount'] == 'Hamısı') {
+            $roomCount = ['1', '2', '3', '4', '5'];
+        }
+        else {
+            $roomCount = [$fields['roomCount']];
+        }
+
+        if($fields['city'] == 'Hamısı') {
+            $city = ['Bakı', 'Sumqayıt', 'Gəncə'];
+        }
+        else {
+           $city = [$fields['city']];
+        }
+
+        $priceMin = $fields['priceMin'];
+        $priceMax = $fields['priceMax'];
+
+        // if(empty($request->roomCount)) {
+        //     $roomCount = 1
+        // }
+        
+        // $posts = Post::where([
+        //     ['trade_type', $tradeType],
+        //     ['estate_type', $estateType],
+        //     ['room_count', $roomCount],
+        //     ['city', $city],
+        //     ['price', '>=', $priceMin],
+        //     ['price', '<=', $priceMax]
+        // ]);
+
+
+        $posts = Post::search($tradeType, $estateType, $roomCount, $city, $priceMin, $priceMax)->latest();
+
+        $vipPosts = Post::search($tradeType, $estateType, $roomCount, $city, $priceMin, $priceMax)->vipPosts()->inRandomOrder();
+        
+        $voucherPosts = Post::search($tradeType, $estateType, $roomCount, $city, $priceMin, $priceMax)->voucherPosts()->inRandomOrder();
+
+        $searchResult['allPostsCount'] = $posts->count();
+        $searchResult['vipPostsCount'] = $vipPosts->count();
+        $searchResult['voucherPostsCount'] = $voucherPosts->count();
+
+        $searchResult['allPosts'] = $posts->simplePaginate(20);
+        $searchResult['vipPosts'] = $vipPosts->simplePaginate(20);
+        $searchResult['voucherPosts'] = $voucherPosts->simplePaginate(20);
+
+        $time = microtime(true) - $start;
+        $searchResult['time'] = $time;
+        
+        return response()->json($searchResult, 200, [/*headers here*/], JSON_PRETTY_PRINT);
     }
 }
