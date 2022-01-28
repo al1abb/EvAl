7e3b2750890e71104fec4d60bcc2c43f9f89c7c0
Ali Abbasov <ali3abbasov@gmail.com>
2022-01-28 02:38:19 +0200

    fix: Update baseUrl in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true;
-// window.axios.defaults.baseURL = 'https://ev-al.herokuapp.com';
+window.axios.defaults.baseURL = 'https://ev-al.herokuapp.com';
 
 
 /**
