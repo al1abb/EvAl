8d18f607cb4818a019c7d97657995e5201e50b8e
Ali Abbasov <ali3abbasov@gmail.com>
2022-04-03 12:18:26 +0300

    refactor: Update bootstrap js file for production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,10 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true;
-// window.axios.defaults.baseURL = 'https://ev-al.herokuapp.com';
+// window.axios.defaults.baseURL = 'http://eval.test';
+
+// for production
+window.axios.defaults.baseURL = 'https://ev-al.herokuapp.com';
 
 
 /**
